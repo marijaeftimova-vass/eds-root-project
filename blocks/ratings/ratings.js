7d8e7be44025ceac2193d7ce51d1854e7bb54576
Linus Kohler <linus.kohler@one-inside.com>
2024-06-13 13:49:02 +0200

    Rating element now respects the max count as well

--- blocks/ratings/ratings.js
+++ blocks/ratings/ratings.js
@@ -1,12 +1,34 @@
 import { createOptimizedPicture } from '../../scripts/aem.js';
 
+function getRatingValue(regex, content) {
+  const matchForFirstNumber = content.match(regex);
+
+  let rating = 0;
+  if (matchForFirstNumber) {
+    rating = parseInt(matchForFirstNumber[1], 10);
+
+    if (rating > 10) {
+      rating = 10;
+    }
+
+    if (rating < 1) {
+      rating = 1;
+    }
+  }
+
+  return rating;
+}
+
 function isRatingElement(element) {
   const content = element.innerHTML;
-  const regex = /^(\d+)\/\d+$/;
 
-  const match = content.match(regex);
+  const regexFirstNumber = /^(\d+)\/\d+$/;
+  const regexSecondNumber = /^\d+\/(\d+)$/;
 
-  return match ? parseInt(match[1], 10) : null;
+  const rating = getRatingValue(regexFirstNumber, content);
+  const maxRating = getRatingValue(regexSecondNumber, content);
+
+  return ((rating !== 0) && (maxRating !== 0)) ? [rating, maxRating] : null;
 }
 
 function isQuoteElement(element) {
@@ -37,10 +59,10 @@ function decorateReview(ratingReviewContainer) {
 
       reviewChild.classList.add('rating-stars');
 
-      for (let i = 0; i < 5; i += 1) {
+      for (let i = 0; i < ratingCount[1]; i += 1) {
         const star = document.createElement('span');
 
-        if (i < ratingCount) {
+        if (i < ratingCount[0]) {
           star.className = 'star-active';
         } else {
           star.className = 'star-inactive';
