6bf632bbd42d46531488f889a865c6536003c0ad
Cornelius Bolten <cornelius.bolten@vasscompany.com>
2025-06-13 15:36:44 +0200

    chore(cards): add icon-cards

--- blocks/cards/cards.js
+++ blocks/cards/cards.js
@@ -1,38 +1,53 @@
 import { createOptimizedPicture } from '../../scripts/aem.js';
 
-function decorateTags(div) {
-  [...div.children].forEach((child) => {
-    const regex = /\[([^\]]+)]/;
-    const content = child.textContent;
+function decorateTags(div, isIconCard) {
+  if (isIconCard) {
+    div.classList.add('icon-card');
+    const [pictureP, headline, contentP] = div.children;
+    const picture = pictureP.querySelector('picture');
+    const flex = document.createElement('div');
+    flex.className = 'icon-header';
+    flex.append(picture, headline);
 
-    const match = content.match(regex);
+    div.append(flex);
+    div.append(contentP);
+  } else {
+    [...div.children].forEach((child) => {
+      const regex = /\[([^\]]+)]/;
+      const content = child.textContent;
 
-    if (match) {
-      child.classList.add('card-tag-wrapper');
+      const match = content.match(regex);
 
-      const span = document.createElement('span');
-      span.textContent = match[1].toString();
-      span.classList.add('card-tag');
+      if (match) {
+        child.classList.add('card-tag-wrapper');
 
-      child.textContent = '';
+        const span = document.createElement('span');
+        span.textContent = match[1].toString();
+        span.classList.add('card-tag');
 
-      child.appendChild(span);
-    }
-  });
+        child.textContent = '';
+
+        child.appendChild(span);
+      }
+    });
+  }
 }
 
 export default function decorate(block) {
   /* change to ul, li */
+  const isIconCard = block.classList.contains('icon-cards');
+
   const ul = document.createElement('ul');
   [...block.children].forEach((row) => {
     const li = document.createElement('li');
     while (row.firstElementChild) li.append(row.firstElementChild);
+
     [...li.children].forEach((div) => {
       if (div.children.length === 1 && div.querySelector('picture')) {
         div.className = 'cards-card-image';
       } else {
         div.className = 'cards-card-body';
-        decorateTags(div);
+        decorateTags(div, isIconCard);
       }
     });
     ul.append(li);
