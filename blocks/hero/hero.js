0705247598f65794697816790bb18e16788074b0
Cornelius Bolten <cornelius.bolten@vasscompany.com>
2025-06-13 15:00:58 +0200

    chore(hero): add grid layout

---
+++ blocks/hero/hero.js
@@ -0,0 +1,7 @@
+// export default async function decorate(block) {
+//   const [innerDiv] = block.children;
+//   const [innerInnerDiv] = innerDiv.children;
+//   const [headline, picture, button] = innerInnerDiv.children;
+//
+//   // innerInnerDiv.textContent = '';
+// }
